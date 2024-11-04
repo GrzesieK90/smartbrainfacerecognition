29378bf3d20da630a4e819c4bdd1ee90f912abe8
GrzesieK90 <109867479+GrzesieK90@users.noreply.github.com>
2024-11-04 15:01:23 +0100

    Changes due to new backend server

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
 import SignIn from './Components/SignIn/SignIn';
 import Register from './Components/Register/Register';
-import Logo from './Components/Logo/Logo'
+import Logo from './Components/Logo/Logo';
 import ImgLinkForm from './Components/ImgLinkForm/ImgLinkForm';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Rank from './Components/Rank/Rank';
 import Particles from './Components/Particles/Particles';
 import './App.css';
 
-
 const initialState = {
   input: '',
-  imageUrl:'',
-  box:{},
-  route:'signin',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
   isSignedIn: false,
   user: {
     id: '',
@@ -23,24 +22,26 @@ const initialState = {
     entries: 0,
     joined: ''
   }
-}
+};
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = initialState;
   }
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    }})
-  }
-  
+    this.setState({
+      user: {
+        id: data._id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    });
+  };
+
   calcFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
@@ -51,55 +52,62 @@ class App extends Component {
       topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
+    };
+  };
 
   dispFaceBox = (box) => {
-    this.setState({box: box})
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (e) => {
-    this.setState({input: e.target.value})
-  }
+    this.setState({ input: e.target.value });
+  };
 
   onPictureSubmit = () => {
-    this.setState({imageUrl: this.state.input});
+    this.setState({ imageUrl: this.state.input });
     fetch('http://localhost:3001/imageurl', {
       method: 'post',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         input: this.state.input
       })
     })
     .then(response => response.json())
     .then(response => {
-      if(response) {
-        fetch('http://localhost:3001/image',{
+      if (response) {
+        fetch('http://localhost:3001/image', {
           method: 'put',
-          headers: {'Content-Type': 'application/json'},
+          headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
-            id: this.state.user.id
+            id: this.state.user.id // Upewnij się, że ID użytkownika jest poprawne
           })
         })
-        .then(response => response.json())
-        .then(count => {
-          this.setState(Object.assign(this.state.user, { entries: count }))
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
         })
-        .catch(console.log)
+        .then(count => {
+          this.setState(prevState => ({
+            user: { ...prevState.user, entries: count }
+          }));
+        })
+        .catch(err => console.log('Error updating entry count:', err));
       }
-      this.dispFaceBox(this.calcFaceLocation(response))
+      this.dispFaceBox(this.calcFaceLocation(response));
     })
-    .catch(err => console.log(err));
-  }
+    .catch(err => console.log('Error in fetching API:', err));
+  };
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState(initialState)
+      this.setState(initialState);
     } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route})
-  }
+    this.setState({ route: route });
+  };
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
@@ -108,18 +116,17 @@ class App extends Component {
         <div className="particles">
           <Particles />
         </div>
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
-        { route === 'home'
-        ? <div> 
-        <Logo />
-        <Rank name={this.state.user.name} entries={this.state.user.entries}/>
-        <ImgLinkForm onInputChange={this.onInputChange} onPictureSubmit={this.onPictureSubmit}/>
-        <FaceRecognition box={box} imageUrl={imageUrl}/>
-      </div>
-        : ( 
-        route === 'signin'
-        ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-        : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === 'home'
+          ? <div>
+              <Logo />
+              <Rank name={this.state.user.name} entries={this.state.user.entries} />
+              <ImgLinkForm onInputChange={this.onInputChange} onPictureSubmit={this.onPictureSubmit} />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
+            </div>
+          : (route === 'signin'
+            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
         }
       </div>
--- src/Components/ImgLinkForm/ImgLinkForm.js
+++ src/Components/ImgLinkForm/ImgLinkForm.js
@@ -1,23 +1,23 @@
 import React from "react";
-import './ImgLinkForm.css'
+import './ImgLinkForm.css';
 
 const ImgLinkForm = ({ onInputChange, onPictureSubmit }) => {
-    return (
-        <div>
-            <p className="text">
-                {'This Smart Brain will detect face on your photo. Give it a try!'}
-            </p>
-            <div className="center">
-                <div className="style1">
-                    <input className="inp" type='text' onChange={onInputChange} />
-                    <button 
-                    className="btn" 
-                    onClick={onPictureSubmit}
-                    >DETECT</button>
-                </div>
-            </div>
+  return (
+    <div>
+      <p className="text">
+        {'This Smart Brain will detect face on your photo. Give it a try!'}
+      </p>
+      <div className="center">
+        <div className="style1">
+          <input className="inp" type='text' onChange={onInputChange} />
+          <button 
+            className="btn" 
+            onClick={onPictureSubmit}
+          >DETECT</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ImgLinkForm
+export default ImgLinkForm;
